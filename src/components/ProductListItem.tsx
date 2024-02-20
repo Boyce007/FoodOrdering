67dc98e535cedb8eb5a3e50b990e3dc11e847618
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-02-19 22:58:24 -0500

    Added stack navigation:

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet,Text,View,Image } from 'react-native';
+import { StyleSheet,Text,View,Image,Pressable } from 'react-native';
 import Colors from "@constants/Colors";
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 type ProductListItemProps = {
     product:Product;
@@ -10,17 +11,20 @@ export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaw
 const ProductListItem = ({product}: ProductListItemProps)=> {
 
     return (
-      <View style={styles.container}>
+      <Link href={`/menu/${product.id}`} asChild>
+        <Pressable style={styles.container}>
         <Image source={{uri:product.image || defaultPizzaImage}}
-         style = {styles.image}
-         resizeMode="contain"
-         /> 
+            style = {styles.image}
+            resizeMode="contain"
+            /> 
         <Text style={styles.title}>{product.name}</Text>
         <Text style={styles.price}>${product.price}</Text>
-      </View>
+        
+        </Pressable>
+      </Link>
     );
   
-  }
+  } 
 
   export default ProductListItem
 
